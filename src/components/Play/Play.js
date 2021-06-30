3110a3a19599287a28c1558f2cfa986163bdb901
mricom <mricomorillo@gmail.com>
2021-06-30 02:36:15 +0200

    Implemented functionality that gets data from the question response and stores it in sessionStorage. Changed buttons css.

--- src/components/Play/Play.js
+++ src/components/Play/Play.js
@@ -1,28 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { fetchSessionToken } from '../../App';
+import ReactLoading from "react-loading";
+import { useHistory } from "react-router-dom";
+import Question from '../Question/Question';
+
 
 export default function Play(){
-  const [question, setQuestion] = useState([]);
+  const history = useHistory();
+  const [loading, setLoading] = useState(true);
+  const userCategory = JSON.parse(window.sessionStorage.getItem('userCategory'));
+  const userDifficulty = JSON.parse(window.sessionStorage.getItem('userDifficulty'));
+
+  
+  const renameKey = (object, oldKey, newKey) => {
+    Object.defineProperty(object, newKey,
+    Object.getOwnPropertyDescriptor(object, oldKey));
+    delete object[oldKey];
+    return object;
+  }
 
   useEffect( () => {
     const sessionToken = window.localStorage.getItem('triviaSessionToken');
     if(!sessionToken){
-
+      fetchSessionToken();
     }
     const fetchData = async () => {
-      const response = await axios('https://opentdb.com/api_token.php?amount=1&token=' + sessionToken);
-      if(response.result_code != 0){
-        console.log('error', response.result_code );
-      } else {
-        setQuestion(response.results);
-      }
+      let url = 'https://opentdb.com/api.php?amount=1&token=' + sessionToken
+      if(userCategory && userCategory !== 0){ url += '&category=' + userCategory}
+      if(userDifficulty){url += '&difficulty=' + userDifficulty.toLowerCase()}
+      const response = axios.get(url)
+        .then(res =>{
+          const code = res.data.response_code;
+          console.log(res.data.response_code);
+          if(code !== 0){
+            if(code === 1){
+              alert("There are no questions with the selected criteria. Please, change the game configuration.");
+              history.push('/game-configuration/');
+            } else if(code === 3 || code===4){
+              fetchSessionToken();
+              history.push(history.location);
+            } else {
+              alert('An error occurred. Sorry for the inconvenience.');
+              history.push('/');
+            }
+          } else {
+            let question = res.data.results[0];
+            question = renameKey(question, 'correct_answer', 'correctAnswer');
+            question = renameKey(question, 'incorrect_answers', 'incorrectAnswers');
+            let choices = question.incorrectAnswers;
+            if(!question.incorrectAnswers.find(elem => elem===question.correctAnswer)){
+              choices.push(question.correctAnswer);
+              choices.sort(() => Math.random() - 0.5);
+            }
+            for(const prop in question){
+              window.sessionStorage.setItem(prop,JSON.stringify(question[prop]));
+            }
+          }
+        }). finally(function(){
+          setLoading(false);
+        });
     };
     fetchData(); 
+    return () => window.sessionStorage.clear();
   }, []);
+
 
   return (
     <>
-
+      <div className="loader-container">
+        <ReactLoading type={'spinningBubbles'} color="#5ac75d" />
+      </div>
+        :
+        <>
+        <Question></Question>
+        </>
     </>
   )
 }
---
+++ src/components/Question/Question.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import ReactLoading from "react-loading";
+
+export default function Question(){
+    return (
+        <>
+        </>
+    )
+}
